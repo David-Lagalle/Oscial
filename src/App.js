b3fb1062e012f7c5e18fe18ba76fad7ac529aab7
David Lagalle <david.lagalle14@gmail.com>
2020-02-10 11:40:04 +0100

    Set rgpd as default component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import { registerRootComponent } from "expo";
 import { StyleSheet } from "react-native";
 import Rgpd from "./components/Rgpd";
-import Login from "./components/Login";
 
 class App extends React.Component {
   render() {
-    return <Login />;
+    return <Rgpd />;
   }
 }
 
