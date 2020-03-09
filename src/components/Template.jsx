3de1a4cee33ec1c14c0d9ccf60b541dfb3118b8c
solenn <vosport46@gmail.com>
2020-03-09 12:28:37 +0100

    template navbar

---
+++ src/components/Template.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from"@mdi/react";
+import {mdiHome,mdiMagnify,mdiAccountCircle, mdiCalendar, mdiCar, mdiBell, mdiForum, mdiArmFlex} from "@mdi/js"
+import styled from './Templatestyle';
+
+export default class Template extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          <div style={{backgroundColor:'green' ,display:'flex', justifyContent:'space-between', paddingTop:'5px'}}> 
+            <div>
+              <p> logo</p>
+            </div>
+            <div>
+              <Icon color="black" style={{ width: 40 }} path={mdiMagnify} />
+              <Icon color="black" style={{ width: 40 }} path={mdiAccountCircle} />
+            </div> 
+          </div>
+          <div style={{display:'flex', justifyContent:'space-around', paddingTop:'5px', paddingBottom:'5px', borderBottom: '2px solid #000000'}}>
+            <Icon color="black" style={{ width: 40 }} path={mdiHome} />
+            <Icon color="black" style={{ width: 40 }} path={mdiCalendar} />
+            <Icon color="black" style={{ width: 40 }} path={mdiCar} />
+            <Icon color="black" style={{ width: 40 }} path={mdiBell} />
+            <Icon color="black" style={{ width: 40 }} path={mdiForum} />
+          </div>
+       </Text> 
+    </View>
+    );
+  }
+}
